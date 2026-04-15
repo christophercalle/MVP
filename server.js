7f8936950c21f93f6f74f3e3f23b21d6e663fb9e
Christopher Calle <33294536+christophercalle@users.noreply.github.com>
2026-04-14 22:09:56 -0500

    Phase 1: Working Express API with SQLite persistence

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const app = express();
+
+app.use(express.json());
+
+// Initialize Database
+const db = new sqlite3.Database('./database.sqlite');
+db.serialize(() => {
+    db.run("CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT)");
+})
+
+
+
+
+// POST: Create a task
+app.post('/tasks', (req, res) => {
+  const { title } = req.body;
+  db.run("INSERT INTO tasks (title) VALUES (?)", [title], function(err) {
+    res.status(201).json({ id: this.lastID, title });
+  });
+});
+
+// GET: Read all tasks
+app.get('/tasks', (req, res) => {
+  db.all("SELECT * FROM tasks", [], (err, rows) => {
+    res.json(rows);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('The server is alive! Go to /tasks to see your data.');
+});
+
+
+app.listen(3000, () => console.log('API running at http://localhost:3000'));
