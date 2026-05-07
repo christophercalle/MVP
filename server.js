23b26b0246f6a8bdeec115a9d0c4b1e41484745e
Christopher Calle <33294536+christophercalle@users.noreply.github.com>
2026-05-06 23:23:34 -0500

    feat: establish sqlite database connection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');  /*  Provides methods to handle web traffic like GET and POST requests.  */
-const sqlite3 = require('sqlite3'); /*  Allows JS to talk to a db file on your hard drive */
-const cors = require('cors'); /*  Allows browser to talk to the backend */
+const sqlite3 = require('sqlite3');  /*  Allows JS to talk to a db file on your hard drive */
+const cors = require('cors');        /*  Allows browser to talk to the backend */
 
 const app = express();
+const db = new sqlite3.Database('database.sqlite');
