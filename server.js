ee7462fde41072ec7126791b9c5d894ca5eadf77
Christopher Calle <33294536+christophercalle@users.noreply.github.com>
2026-05-23 23:49:46 -0500

    feat: add cors and express json middleware

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-/* 1. IMPORTS */
+/* IMPORTS */
 const express = require('express');
 const cors = require('cors');
 
 /* INITIALIZATION */
 const app = express();
+
+/* MIDDLEWARE */ 
+app.use(cors());
+app.use(express.json());
